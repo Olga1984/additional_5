c895c8c46dccc6110b7b08ad31a1d6c75b2b8b93
Olga1984 <noa84@tut.by>
2017-09-26 04:50:30 +0300

    check: basic implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    var openingChars = [];
+    var closingChars = [];
+
+    for(var i = 0; i<bracketsConfig.length; i++){
+    openingChars.push(bracketsConfig[i][0]);
+    //console.log(bracketsConfig[i][0]+'c1');   
+    closingChars.push(bracketsConfig[i][1]);
+    //console.log(bracketsConfig[i][1]+'c2');
+    }
+var stack = [];
+for (var j = 0, len = str.length; j < len; j++) {
+    var char = str[j];
+    var openIdx = openingChars.indexOf(char);
+    console.log(openIdx+'open');
+    var closeIdx = closingChars.indexOf(char);
+   console.log(closeIdx+'close');
+   if (openIdx == closeIdx){
+        continue;
+    }else if (openIdx > -1) {
+    stack.push(openIdx);
+       console.log(stack+'stack-open');
+    } 
+    else if (closeIdx > -1) {
+        if (stack.length === 0) return false;
+        var lastIdx = stack.pop();
+        console.log(stack+'stack-last');
+        console.log(lastIdx);
+        if (lastIdx !== closeIdx) return false;
+    }
+}
+
+if (stack.length !== 0) return false;
+return true;
 }
