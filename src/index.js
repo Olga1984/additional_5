ca126587e213733c29c535151b2cb0e31865662e
Olga1984 <noa84@tut.by>
2017-09-29 23:43:40 +0300

    check: replace if with continue to if i first for loop to add '{' and '}'

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-    var openingChars = [];
-    var closingChars = [];
-
-    for(var i = 0; i<bracketsConfig.length; i++){
-    openingChars.push(bracketsConfig[i][0]);
-    //console.log(bracketsConfig[i][0]+'c1');   
-    closingChars.push(bracketsConfig[i][1]);
-    //console.log(bracketsConfig[i][1]+'c2');
+module.exports =    function check(str, bracketsConfig) {
+        // your solution
+        var openingChars = [];
+        var closingChars = [];       
+        for(var i = 0; i<bracketsConfig.length; i++){
+            if (bracketsConfig[i][0] === bracketsConfig[i][1])
+    {
+        bracketsConfig[i][0] = '{';
+        bracketsConfig[i][1] = '}';
+        //console.log(bracketsConfig[i][0]);
+        //console.log(bracketsConfig[i][1]);
     }
-var stack = [];
-for (var j = 0, len = str.length; j < len; j++) {
-    var char = str[j];
-    var openIdx = openingChars.indexOf(char);
-    console.log(openIdx+'open');
-    var closeIdx = closingChars.indexOf(char);
-   console.log(closeIdx+'close');
-   if (openIdx == closeIdx){
-        continue;
-    }else if (openIdx > -1) {
-    stack.push(openIdx);
-       console.log(stack+'stack-open');
-    } 
-    else if (closeIdx > -1) {
-        if (stack.length === 0) return false;
-        var lastIdx = stack.pop();
-        console.log(stack+'stack-last');
-        console.log(lastIdx);
-        if (lastIdx !== closeIdx) return false;
+            openingChars.push(bracketsConfig[i][0]);
+            console.log(bracketsConfig[i][0]+'c1');
+            closingChars.push(bracketsConfig[i][1]);
+            console.log(bracketsConfig[i][1]+'c2');                        
+        }       
+        var stack = [];
+        for (var j = 0, len = str.length; j < len; j++) {
+            var char = str[j];
+            var openIdx = openingChars.indexOf(char);
+            //console.log(openIdx+'-open');
+            var closeIdx = closingChars.indexOf(char);
+            //console.log(closeIdx+'-close');
+            if (openIdx !== -1) {
+                stack.push(openIdx);
+                //console.log(stack);              
+                //console.log(stack+'stack-open');
+            }
+            else if (closeIdx !== -1) {               
+                if (stack.length === 0) return false;
+                var lastIdx = stack.pop();
+                // console.log(stack+'stack-last');
+                //console.log(lastIdx+'-lastIdx');
+                if (lastIdx !== closeIdx) return false;
+            }
+        }
+//console.log(stack+'-stack');
+        if (stack.length !== 0) return false;
+        return true;
     }
-}
-
-if (stack.length !== 0) return false;
-return true;
-}
